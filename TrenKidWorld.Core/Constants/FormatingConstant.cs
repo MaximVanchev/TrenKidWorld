fb9c3d8899821a86a9932418365e39c9476fd5a9
Maxim Vanchev <maxim.van.mv@gmail.com>
2023-11-21 23:09:46 +0200

    Add project files.

---
+++ TrenKidWorld.Core/Constants/FormatingConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrenKidWorld.Core.Constants
+{
+    public class FormatingConstant
+    {
+        public const string NormalDateConstant = "dd.MM.yyyy";
+    }
+}
---
+++ TrenKidWorld.Core/Constants/MessageConstant.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TrenKidWorld.Core.Constants
+{
+    public static class MessageConstant
+    {
+        public const string SuccessMessage = "SuccessMessage";
+        public const string ErrorMessage = "ErrorMessage";
+        public const string WarningMessage = "WarningMessage";
+    }
+}
